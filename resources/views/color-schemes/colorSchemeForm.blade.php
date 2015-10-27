57f451cbd0906504b6565fd0b252787718c34a83
Andy Gott <andy@madebyfieldwork.com>
2015-10-27 14:04:25 +0000

    Use HTML color input for color schemes.

--- resources/views/color-schemes/colorSchemeForm.blade.php
+++ resources/views/color-schemes/colorSchemeForm.blade.php
@@ -1,22 +1,22 @@
 <!-- Color 1 -->
-{!! Form::label('color_1', 'Colour 1') !!}
-{!! Form::text('color_1') !!}
+{!! Form::label('color_1', 'Background') !!}
+{!! Form::input('color', 'color_1') !!}
 
 <?php if ($errors->first('color_1')) { ?>
     <p><?php echo $errors->first('color_1') ?></p>
 <?php } ?>
 
 <!-- Color 2 -->
-{!! Form::label('color_2', 'Colour 2') !!}
-{!! Form::text('color_2') !!}
+{!! Form::label('color_2', 'Primary') !!}
+{!! Form::input('color', 'color_2') !!}
 
 <?php if ($errors->first('color_2')) { ?>
     <p><?php echo $errors->first('color_2') ?></p>
 <?php } ?>
 
 <!-- Color 3 -->
-{!! Form::label('color_3', 'Colour 3') !!}
-{!! Form::text('color_3') !!}
+{!! Form::label('color_3', 'Secondary') !!}
+{!! Form::input('color', 'color_3') !!}
 
 <?php if ($errors->first('color_3')) { ?>
     <p><?php echo $errors->first('color_3') ?></p>

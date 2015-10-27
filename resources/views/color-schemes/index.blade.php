916fcb03c9e2ea72fa66ad3b7595bd6f97535e78
Andy Gott <andy@madebyfieldwork.com>
2015-10-27 14:09:16 +0000

    Improve color schemes index UI.

--- resources/views/color-schemes/index.blade.php
+++ resources/views/color-schemes/index.blade.php
@@ -5,12 +5,20 @@
 @section('content')
     <h2>Color Schemes</h2>
 
+    <a href="{{ route('color-schemes.create') }}">Add a new colour scheme</a>
+
     @if ( !$colorSchemes->count() )
         There are no color schemes :(
     @else
         <ul>
             @foreach($colorSchemes as $cs)
-                <li><a href="{{ route('color-schemes.edit', $cs->id) }}">{{ $cs->color_1 }}, {{ $cs->color_2 }}, {{ $cs->color_3 }}</a></li>
+                <li>
+                    <a class="color-scheme" href="{{ route('color-schemes.edit', $cs->id) }}">
+                        <span class="color-scheme-color" style="background-color: {{ $cs->color_1 }}">{{ $cs->color_1 }}</span>
+                        <span class="color-scheme-color" style="background-color: {{ $cs->color_2 }}">{{ $cs->color_2 }}</span>
+                        <span class="color-scheme-color" style="background-color: {{ $cs->color_3 }}">{{ $cs->color_3 }}</span>
+                    </a>
+                </li>
             @endforeach
         </ul>
     @endif

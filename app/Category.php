7ed8f21d33e8a1a3710c133de421b2ce91c0e196
Andy Gott <andy@madebyfieldwork.com>
2015-10-28 17:13:23 +0000

    Provide a method for getting the event count for a category.

--- app/Category.php
+++ app/Category.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Event;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\SluggableInterface;
 use Cviebrock\EloquentSluggable\SluggableTrait;
@@ -41,5 +42,14 @@ class Category extends Model implements SluggableInterface
     public function colorScheme()
     {
         return $this->belongsTo('App\ColorScheme');
+    }
+
+    public function eventCount() 
+    {
+        if (!isset($this->_eventCount)) {
+            $this->_eventCount = Event::where('category_id', $this->id)->where('time_end', '>=', date('Y-m-d H:i:s'))->count();
+        }
+
+        return $this->_eventCount;
     }
 }

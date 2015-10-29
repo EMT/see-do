078bc72d89ea5646c452d13cbe1233e604fea131
Harry Parton <hparton@hotmail.co.uk>
2015-10-29 10:15:03 +0000

    Re-add jQuery link in header

--- resources/views/common/js.blade.php
+++ resources/views/common/js.blade.php
@@ -1,2 +1,2 @@
-{{-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> --}}
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="{{ elixir('js/all.js') }}"></script>

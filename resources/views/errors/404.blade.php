ba368e8b47f0051ccd0ea74dc1112e8723bcd41e
Andy Gott <andy@madebyfieldwork.com>
2015-11-02 23:45:56 +0000

    Add 404 page. Closes #31

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title', 'Page Not Found — See&Do')
+
+@section('content')
+    <h2 class="aligned">404</h2>
+
+    <div class="aligned">
+        <p>Page not found.</p>
+    </div>
+@stop

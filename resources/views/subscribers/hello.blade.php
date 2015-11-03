61d572a5f242c80276e86e1e4d3deb65aaf11fff
Andy Gott <andy@madebyfieldwork.com>
2015-11-03 10:43:52 +0000

    Improved message page templates. Closes #28

--- resources/views/subscribers/hello.blade.php
+++ resources/views/subscribers/hello.blade.php
@@ -5,7 +5,9 @@
 @section('content')
     <h2 class="aligned">Hello</h2>
 
-    <div class="aligned">
-        <p>Thanks for subscribing. We’ve sent you a confirmation email. You don’t need to do anything with it, but if you want to update your settings follow the link there to find your settings page.</p>
+    <div class="article-body aligned">
+        <p>Thanks for subscribing. We’ll send you a weekly round-up of upcoming things to See&Do.</p>
+
+        <p>We’ve sent you a confirmation email. You don’t need to do anything with it, but if you ever need to update your settings or unsubscribe you can follow the links in the email.</p>
     </div>
 @stop
--- resources/views/subscribers/updated.blade.php
+++ resources/views/subscribers/updated.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <h2 class="aligned">Settings Updated</h2>
 
-    <div class="aligned">
+    <div class="article-body aligned">
         <p>Your settings have been updated.</p>
     </div>
 @stop

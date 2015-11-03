885514f101e01fe46fb7394c0254932ff02b146d
Andy Gott <andy@madebyfieldwork.com>
2015-11-03 11:55:57 +0000

    Add icons table.

---
+++ database/migrations/2015_11_03_115419_create_icons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIconsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('icons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('icon');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('icons');
+    }
+}

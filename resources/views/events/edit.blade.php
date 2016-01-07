ed8d9bcadc6ba0eb6ce898377fa2940b57913499
Harry Parton <hparton@hotmail.co.uk>
2016-01-07 14:35:04 +0000

    Add delete button to the event edit page

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -6,8 +6,16 @@
     <h2 class="aligned">Edit Event</h2>
 
     {!! Form::model($event, ['route' => ['events.update', $event->slug], 'method' => 'put', 'class' => 'form']) !!}
-		
+
 		@include('events.eventForm')
 
     {!! Form::close() !!}
+	<div class="form no-mgt">
+		<div class="form-row">
+		    <div class="form-row-body">
+		        {!! delete_form(['events.destroy', $event->slug]) !!}
+		    </div>
+		</div>
+	</div>
+
 @stop

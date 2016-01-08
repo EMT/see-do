b2636e20f27c44d3d4f6333f142a5cf41838cb55
Harry Parton <hparton@hotmail.co.uk>
2016-01-08 15:41:13 +0000

    Implement tweet when adding an event

--- app/Listeners/SocialBroadcast.php
+++ app/Listeners/SocialBroadcast.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use Log;
+use Twitter;
 use App\Events\EventPosted;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -28,6 +29,18 @@ class SocialBroadcast
     public function handle(EventPosted $event)
     {
         Log::info('Fired event for new event: '.$event->event);
+
+        $event = $event->event;
+
+        $title = $event->title;
+        $date = date('d/m/y', strtotime($event->time_start));
+        $time = date('g.ia', strtotime($event->time_start));
+        $venue = explode(",", $event->venue)[0];
+        $link = route('events.show', ['slug' => $event->slug]);
+
+        // Twitter::postTweet(array('status' => $title . ' on ' . $date . ' - '. $time .' at '. $venue . ' : ' . $link, 'format' => 'json'));
+
+        Log::info($title . ' on ' . $date . ' - '. $time .' at '. $venue . ' : ' . $link);
 
         // Add in the twitter/facebook broadcast options
     }

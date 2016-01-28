3cb67048a8d78b99e5473eade63d9ace07211020
Harry Parton <hparton@hotmail.co.uk>
2016-01-28 15:03:03 +0000

    Add migration to drop colour scheme from categories

---
+++ database/migrations/2016_01_28_145923_remove_color_scheme_from_categories.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveColorSchemeFromCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn('color_scheme_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->integer('color_scheme_id');
+        });
+    }
+}

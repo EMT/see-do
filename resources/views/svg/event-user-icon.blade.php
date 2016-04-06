f9aa3b0ed0054335c6e128eef974d9100deebd62
Harry Parton <hparton@hotmail.co.uk>
2016-04-06 16:22:40 +0100

    Update user icon

--- resources/views/svg/event-user-icon.blade.php
+++ resources/views/svg/event-user-icon.blade.php
@@ -1,9 +1,13 @@
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     viewBox="0 0 15 15" style="enable-background:new 0 0 15 15;" xml:space="preserve">
+	 viewBox="0 0 16.2 16.2" style="enable-background:new 0 0 16.2 16.2;" xml:space="preserve">
 <g>
-<path class="event-secondary-color" d="M1.4,14.6c-0.3,0-0.6-0.1-0.8-0.3c-0.2-0.2-0.4-0.6-0.3-0.9L1.6,7c0.1-0.7,0.5-1.4,1-1.9l4.9-4.9l7.3,7.3
-	l-4.9,4.9c-0.5,0.5-1.2,0.9-1.9,1l-6.4,1.2C1.6,14.6,1.5,14.6,1.4,14.6z M7.5,1.6L3.3,5.8C2.9,6.2,2.6,6.7,2.5,7.2l-1.2,6.4l0.2,0.6
-	l-0.1-0.5l6.4-1.2c0.5-0.1,1-0.4,1.4-0.7l4.2-4.2L7.5,1.6z"/>
-<rect x="5.4" y="4.6" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -4.5933 6.8256)" class="event-secondary-color" width="1" height="8.7"/>
+	<g>
+		<g>
+			<path class="event-secondary-color" d="M9,15.7H0.5V0.5h15.2V9L9,15.7z M1.5,14.7h7.2l6.1-6.1V1.5H1.5V14.7z"/>
+		</g>
+		<g>
+			<polygon class="event-secondary-color" points="8.8,15 7.9,15 7.9,8.1 15.2,8.1 15.2,9.1 8.8,9.1 			"/>
+		</g>
+	</g>
 </g>
 </svg>

cef5da7f6717d976edc80a1804d77c63c1767c27
Harry Parton <hparton@hotmail.co.uk>
2016-05-24 17:11:25 +0100

    Add middleware to stop users registered with a city editing another cities info

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\City;
+use Notification;
 use Illuminate\Contracts\Auth\Guard;
 
 class Authenticate
@@ -36,11 +38,28 @@ class Authenticate
      */
     public function handle($request, Closure $next)
     {
+        // If the user isn't logged in or they are part of a different city
+        // deny access, otherwise go for it. Might be worth adding a message to
+        // explain what happened on redirect.
+        $city = City::findByIATA($request->route()->getParameter('city'))->first();
+
+
         if ($this->auth->guest()) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
+                Notification::error('You need to be logged in to view that.');
+
                 return redirect()->guest('auth/login');
+            }
+        } else if ($this->auth->user()->city_id !== $city->id) {
+
+            Notification::error('You don\'t have permissions for that city.');
+
+            if ($request->ajax()){
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect('/'.$city->iata);
             }
         }
 

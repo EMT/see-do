2219c00879b741284dc2cb2fdd3719f2c0733bb0
Harry Parton <hparton@hotmail.co.uk>
2016-05-31 16:09:42 +0100

    Remove hardcoded references to nyc

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,7 +9,7 @@
                 <h2 class="page-intro-title">{{$user->username}}</h2>
                 <p>{!! nl2br($user->bio) !!}</p>
                 @if (Auth::check())
-                        <a class="link-effect edit-user-details" href="{{ route('{city}.users.edit', ['city' => Request::route()->getParameter('city'), 'users' => $user->slug])}}">Edit your details</a>
+                        <a class="link-effect edit-user-details" href="{{ route('{city}.users.edit', ['city' => Request::route()->getParameter('city')->iata, 'users' => $user->slug])}}">Edit your details</a>
                 @endif
             </div>
         </div>
@@ -21,7 +21,7 @@
             @foreach( $events as $ev )
 
                 <li id="event-item-{{ $ev->id }}" class="event clear {{ ($event && $event->id === $ev->id) ? 'event--active' : '' }}">
-                    <a href="{{ route('{city}.events.show', array('nyc', $ev->slug)) }}">
+                    <a href="{{ route('{city}.events.show', array(Request::route()->getParameter('city')->iata, $ev->slug)) }}">
                         <div class="event-item-date">
                             <span class="meta-data">{!! $ev->shortDates() !!}</span>
                         </div>

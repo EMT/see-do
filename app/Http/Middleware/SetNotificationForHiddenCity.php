a14bd2f1e139d15463bcdaffb61d8a9679cc86b1
Harry Parton <hparton@hotmail.co.uk>
2016-09-16 15:33:19 +0100

    Fix the mailto: link on the warning message

--- app/Http/Middleware/SetNotificationForHiddenCity.php
+++ app/Http/Middleware/SetNotificationForHiddenCity.php
@@ -21,7 +21,7 @@ class SetNotificationForHiddenCity
         $city = $request->route()->getParameter('city');
         if($city) {
             if ($city->hidden) {
-                Notification::warning('This city is currently hidden (it will not be visible unless you are logged in), when you have populated it with a few events you can request it be made public <a href="mailto:harry@madebyfieldwork.com&subject=Unhide '.$city->name.'">here</a>');
+                Notification::warning('This city is currently hidden (it will not be visible unless you are logged in), when you have populated it with a few events you can request it be made public <a href="mailto:harry@madebyfieldwork.com?subject=Unhide%20'.$city->name.'">here</a>');
             }
         }
         return $response;

505360b59551d4f818289c8d6ce47bcde6868420
Harry Parton <hparton@hotmail.co.uk>
2016-09-26 17:29:52 +0100

    Migrating now creates new categories + updates event references

--- database/migrations/2016_09_23_103138_add_city_id_to_categories.php
+++ database/migrations/2016_09_23_103138_add_city_id_to_categories.php
@@ -2,6 +2,7 @@
 
 use App\Category;
 use App\City;
+use App\Event;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -19,6 +20,7 @@ class AddCityIdToCategories extends Migration
         });
 
         $cities = City::all();
+        $categories = Category::all();
 
         foreach ($cities as $city) {
             Category::create(['title' => 'Talks', 'city_id' => $city->id]);
@@ -27,9 +29,31 @@ class AddCityIdToCategories extends Migration
             Category::create(['title' => 'Hackdays & Workshops', 'city_id' => $city->id]);
             Category::create(['title' => 'Films', 'city_id' => $city->id]);
             Category::create(['title' => 'Food & Drink', 'city_id' => $city->id]);
+
+            $city_events = Event::all();
+
+            foreach ($city_events as $event) {
+                if ($event->city_id == $city->id) {
+                    // get the current category_id.
+                    // get the category by that id.
+                    $old_category = Category::where('id', $event->category_id)->first();
+                    // get the name of the category.
+                    // find the new category for this city with that name.
+                    $new_category = Category::where('slug', $old_category->slug)->where('city_id', $city->id)->first();
+                    // set the event to new category.
+                    $event->category_id = $new_category->id;
+                    $event->save();
+                }
+            }
         }
 
+        $categories = Category::all();
 
+        foreach ($categories as $category) {
+            if ($category->city_id == 0) {
+                $category->delete();
+            }
+        }
     }
 
     /**

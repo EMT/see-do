82cfbf79063b01873faf70bed2e85951a39cba1a
Harry Parton <hparton@hotmail.co.uk>
2016-09-27 14:24:13 +0100

    Testing only showing recently active collaborators
    #83

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,21 +6,23 @@
 <div class="left-align-wrapper events-list">
 		<div class="page-intro">
 			<div class="page-intro-inner">
-				<h2 class="page-intro-title">Our Collaborators</h2>
+				<h2 class="page-intro-title">Our Active Collaborators</h2>
 				<p>{{ trans('pages.collaborators')}}</p>
 			</div>
 		</div>
 		<ul>
 			@foreach( $users as $user )
-				<li>
-					<a href="{{ route('{city}.users.show', ['city' => Request::route()->getParameter('city'), 'users' => $user->slug])}}" class="user-item">
-						<div class="event-item-title">
-				            <div class="event-item-inner">
-				                <h3>{{ $user->name_first }} {{ $user->name_last }} <span class="events-count">[ {{$user->user_events_count}} ]</span></h3>
-				            </div>
-				        </div>
-				    </a>
-				</li>
+				@if ($user->user_events_count > 0)
+					<li>
+						<a href="{{ route('{city}.users.show', ['city' => Request::route()->getParameter('city'), 'users' => $user->slug])}}" class="user-item">
+							<div class="event-item-title">
+					            <div class="event-item-inner">
+					                <h3>{{ $user->name_first }} {{ $user->name_last }} <span class="events-count">[ {{$user->user_events_count}} ]</span></h3>
+					            </div>
+					        </div>
+					    </a>
+					</li>
+				@endif
 			@endforeach
 		</ul>
 </div>
